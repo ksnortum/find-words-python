dd8fd6c1d1f380786c0806c01d5e300cc2c8f9bf
Knute Snortum <knute@snortum.net>
2022-08-15 08:58:02 -0700

    Create color scheme for menus that match in Linux and Windows

--- main.py
+++ main.py
@@ -3,11 +3,14 @@ import sys
 
 from PyQt5.QtWidgets import QApplication
 from view.main_window import MainWindow
+from utils.utils import get_string_from_file
 
 
 def main():
     logging.basicConfig(format='%(levelname)s %(message)s', level=logging.WARN)
     app = QApplication(sys.argv)
+    css = get_string_from_file("html/app.css")
+    app.setStyleSheet(css)
     window = MainWindow()
     window.show()
     app.exec()
